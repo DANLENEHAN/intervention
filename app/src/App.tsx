01b41c0a73da6d13c960c424b9afae4990a38b55
Dan Lenehan <dan@myurbanjungle.com>
2022-01-24 22:30:11 +0000

    Login Page + Code cleanup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,13 +5,14 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 
 interface User {
-  email?: string;
-  password?: string;
+  email: string,
+  password: string,
+  emailValid?: boolean,
+  passwordValid?: boolean,
 }
 
 
@@ -20,14 +21,12 @@ function App() {
       <Router>
           <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<NameForm/>} />
+            <Route path="/login" element={<NameForm email={''} password={''}/>} />
           </Routes>
       </Router>
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
 
 function Home() {
   return (
@@ -37,15 +36,13 @@ function Home() {
   );
 }
 
-class NameForm extends React.Component<
-  User,
-  User
-> {
+class NameForm extends React.Component<User, User>
+{
   constructor(props: User) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
+      email: this.props.email,
+      password: this.props.password,
     };
 
     this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -55,57 +52,91 @@ class NameForm extends React.Component<
   handleEmailChange(event: any) {
     this.setState({
       email: event.target.value,
-      password: event.target.password,
     });
   }
 
   handleSumbit() {
-      // Simple POST request with a JSON body using fetch
-      const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(this.state)
-      };
-      fetch('http://192.168.1.101:5000', requestOptions)
-          .then((response) => console.log(response.json()))
+    this.checkFormValidity();
+    if (
+      this.state.emailValid &&
+      this.state.passwordValid
+    ) {
+        // Simple POST request with a JSON body using fetch
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(this.state)
+        };
+        fetch('http://192.168.1.101:5000', requestOptions)
+            .then((response) => console.log(response.json()))
+      }
+  }
+
+  checkFormValidity() {
+    if (this.state.email.indexOf("@") === -1) {
+      this.setState({
+        emailValid: false
+      });
+    } else {
+      this.setState({
+        emailValid: true
+      });
+    }
+
+    if (this.state.password.length < 15) {
+      this.setState({
+        passwordValid: false
+      });
+    } else {
+      this.setState({
+        passwordValid: true
+      });
+    }
   }
 
   handlePasswordChange(event: any) {
-    this.setState({password: event.target.value});
+    this.setState(
+      {password: event.target.value
+    });
   }
 
   render() {
+    let errorMessages = []
+    if (this.state.emailValid === false) {
+      errorMessages.push(<div className="margin-bottom" key={0}>EMAIL ERROR</div>);
+    }
+    if (this.state.passwordValid === false) {
+      errorMessages.push(<div className="margin-bottom" key={1}>PASSWORD ERROR</div>);
+    }
+
     return (
       <div className="flex-parent main-container">
         <div className="login-container">
           <div className="flex-parent flex-column">
             <span className="title margin-bottom">
-              Login
+              Create Account
             </span>
             <input 
-              className="margin-bottom"
+              className="input margin-bottom"
               type="text"
               value={this.state.email}
               onChange={this.handleEmailChange}
+              placeholder="email"
             />
             <input
-              className="margin-bottom"
-              type="text"
+              className="input margin-bottom"
+              type="password"
               value={this.state.password}
               onChange={this.handlePasswordChange}
+              placeholder="password"
             />
-
-            <button onClick={() => this.handleSumbit()}>
-              Click me
-            </button>
-
-          <span className="title margin-bottom">
-            Input data
-          </span>
-          <div className="Flex-parent">
-            <div>Email: {this.state.email}</div>
-            <div>Password: {this.state.password}</div>
-          </div>
+            <div
+              className="margin-bottom button"
+              onClick={() => this.handleSumbit()}
+            >
+              Sign Up
+            </div>
+            {errorMessages}
           </div>
         </div>
       </div>
